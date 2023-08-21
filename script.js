2fd23ae7125bb12406373c0af2296be327115f87
Victória Almeida <106677669+ma-vick@users.noreply.github.com>
2023-08-21 19:24:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
 const vick = 'victória';
 const nick = 'nicolas';
 
-function combStart(str){
-    let arrSubstr = [];
+function combs(str) {
+        let arrSubstr = [];
 
-    for(let i = 0; i < str.length; i++){
-        for(let j = i + 1; j <= str.length; j++){
+        for (let i = 0; i < str.length; i++) {
+          for (let j = i + 1; j <= str.length; j++) {
             arrSubstr.push(str.substring(i, j));
+          }
         }
-    }
 
-    return arrSubstr;
-}
+        return arrSubstr;
+      }
 
-function combEnd(str){
-    let arrSubstr = [];
+      function combNames(one, two) {
+        let arrCombNames = [];
 
-    for (let i = str.length; i >= 0; i--) { 
-        for(let j = i; j < str.length; j++){
-            console.log(i, j);
-            arrSubstr.push(str.substring(i, j + 1));
-        }
-    }
-
-    return arrSubstr;
-}
-
-function combNames(one, two){
-    let arrCombNames = [];
-
-    for(let i = 0; i < one.length; i++){
-        for(let j = i; j < two.length; j++){
+        for (let i = 0; i < one.length; i++) {
+          for (let j = 0; j < two.length; j++) {
             let comb = one[i] + two[j];
             arrCombNames.push(comb);
+          }
         }
-    }
 
-    return arrCombNames;
-}
+        return arrCombNames;
+      }
 
-let vickPar = combStart(vick);
-let nickPar = combEnd(nick);
+      let vickPar = combs(vick);
+      let nickPar = combs(nick);
 
 console.log(vickPar);
 console.log(nickPar);
